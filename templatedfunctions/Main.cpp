888e3201b7eda9245048f2796a9d20a41d023100
Dotes <dote@dote.com>
2020-12-06 18:43:33 -0500

    Add project files.

---
+++ templatedfunctions/Main.cpp
@@ -0,0 +1,38 @@
+//John Doty
+//CIS-1202
+//12/6/2020
+//Templated Functions
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+template <class T>
+T divide(T number) {
+	return number / 2;
+}
+
+
+
+int main(){
+
+	int a, b = 3;
+
+	a = divide(b);
+
+	cout << static_cast<float>(round(a)) << endl;
+
+	float c, d = 5;
+
+	c = divide(d);
+
+	cout << c << endl;
+
+	double e, f = 7;
+
+	e = divide(f);
+
+	cout << e << endl;
+
+}
